6f9042810732c5ab7a5a225261af9d440ddca3d5
Kris Dekeyser <kris.dekeyser@libis.be>
2023-02-13 18:30:56 +0100

    Fix: camelized name was not persited properly

--- spec/teneo/tasks/camelize_name.rb
+++ spec/teneo/tasks/camelize_name.rb
@@ -5,6 +5,6 @@ require "teneo/tools/extensions/string"
 class CamelizeName < ::Teneo::Workflow::Task
   def process(item)
     return unless item.is_a?(TestFileItem) || item.is_a?(TestDirItem)
-    item.properties[:name] = item.name.camelize
+    item.name = item.name.camelize
   end
 end
